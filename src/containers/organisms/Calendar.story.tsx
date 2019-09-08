53890cd32b5c60de444a02a6dd8a2b60c90fea7c
igm50 <keisuke.igimi@gmail.com>
2019-09-08 15:03:12 +0900

    add: knobsアドオンを追加

--- src/containers/organisms/Calendar.story.tsx
+++ src/containers/organisms/Calendar.story.tsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import dayjs from 'dayjs'
 import { storiesOf } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
+import { withKnobs, object } from '@storybook/addon-knobs'
 
 import Calendar from './Calendar'
 
 const division = 'containers/organisms/カレンダー'
-const date = dayjs('2020-10-10')
 
-storiesOf(division, module).add('サンプル', () => {
+const stories = storiesOf(division, module)
+
+stories.addDecorator(withKnobs)
+stories.add('サンプル', () => {
+  const defaultDate = dayjs('2020-10-10')
   const dummyDayTaskList = [{ day: 5, tasks: ['タスクタスクタスクタスク'] }]
 
-  return <Calendar date={date} dayTaskList={dummyDayTaskList}></Calendar>
+  return (
+    <Calendar
+      date={object('日付', defaultDate)}
+      dayTaskList={object('日付タスクリスト', dummyDayTaskList)}
+    ></Calendar>
+  )
 })
