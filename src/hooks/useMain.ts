785454e4340d4e270238d9e9b25764dcfca30add
igm50 <keisuke.igimi@gmail.com>
2019-09-21 12:39:55 +0900

    update: 変更不要な値をメモ化

--- src/hooks/useMain.ts
+++ src/hooks/useMain.ts
@@ -4,12 +4,12 @@ import dayjs from 'dayjs'
 import { CalendarProps, DayTasks } from '../containers/organisms/Calendar'
 
 const useMain = () => {
-  const [date, setDate] = useState(dayjs())
+  const nowDate = useMemo(() => dayjs(), [])
   const [dayTaskList, setDayTaskList] = useState<Array<DayTasks>>([])
 
   const calendarProps: CalendarProps = useMemo(() => {
-    return { date: date, dayTaskList: dayTaskList }
-  }, [date, dayTaskList])
+    return { date: nowDate, dayTaskList: dayTaskList }
+  }, [nowDate, dayTaskList])
 
   return calendarProps
 }
